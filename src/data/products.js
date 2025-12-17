376b420e6406acca5cd6a084e251ba032edc30b1
ilyassbnh <benhida.ilyass@gmail.com>
2025-12-17 11:05:33 +0100

    create all the files for the project

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: "Casque Audio Pro",
+    price: 150,
+    image: "https://via.placeholder.com/150",
+    category: "Audio"
+  },
+  {
+    id: 2,
+    name: "Casque Audio Pro",
+    price: 150,
+    image: "https://via.placeholder.com/150",
+    category: "Audio"
+  },
+  {
+    id: 3,
+    name: "Casque Audio Pro",
+    price: 150,
+    image: "https://via.placeholder.com/150",
+    category: "Audio"
+  },
+  {
+    id: 4,
+    name: "Casque Audio Pro",
+    price: 150,
+    image: "https://via.placeholder.com/150",
+    category: "Audio"
+  },
+  {
+    id: 5,
+    name: "Casque Audio Pro",
+    price: 150,
+    image: "https://via.placeholder.com/150",
+    category: "Audio"
+  },
+];
